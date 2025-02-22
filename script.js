75f1f7f8adc5c4fd72674906cf819b3dd81d562a
THESHARK9001 <yarmined@gmail.com>
2025-02-22 14:34:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function myFunction() {
     let myPrompt = prompt("Please enter the answer");
     switch(myPrompt){
-      case "iloveyou":
-          message = "<a href='iloveyou.html'>You got the answer!</a>"
-          break;
+        case "iloveyou":
+            message = "<a href='iloveyou.html'>You got the answer!</a>"
+            break;
+        case "iloveyou.html":
+            message = "<a href='iloveyou.html'>You got the answer!</a>"
+            break;
       default:
           message = "Wrong answer! try again :3"
           break;
